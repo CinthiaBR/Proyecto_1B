bd77a21b1f64a83ee5c41b05c495e76a67582759
CinthiaBR <cintha.burbanopn.edu.ec>
2021-01-17 20:08:00 -0500

    Agregar ejecutable de consultar numero de especialidad

--- src/main/java/ec/edu/epn/proyecto1b/Ejecutable_Consultar.java
+++ src/main/java/ec/edu/epn/proyecto1b/Ejecutable_Consultar.java
@@ -1,9 +1,11 @@
 package ec.edu.epn.proyecto1b;
-
-public class Ejecutable_Consultar {
+class Ejecutable_proyecto {
     public static void  main (String[] arg){
         consultar c= new consultar();
     String nombre=c.especialista(" "), cedula=c.cedula("");
     System.out.println("Datos del especialista: \n"+nombre +"\n"+cedula);
+
+    int numero_especialidad = 2;
+    System.out.println("Número de especialidad: \n"+numero_especialidad);
     }
 }
